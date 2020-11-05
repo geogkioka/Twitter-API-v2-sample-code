6ea3f9829f86d34547f3a3318fdc089be7f1b1a0
Georgia Gkioka <georgiagkioka@192.168.1.2>
2020-11-05 12:43:52 +0200

    Added store to file

--- Tweet-Lookup/get_tweets_with_bearer_token.py
+++ Tweet-Lookup/get_tweets_with_bearer_token.py
@@ -1,6 +1,8 @@
+#!/usr/bin/env python3
 import requests
 import os
 import json
+
 
 # To set your enviornment variables in your terminal run the following line:
 # export 'BEARER_TOKEN'='<your_bearer_token>'
@@ -49,6 +51,8 @@ def main():
     headers = create_headers(bearer_token)
     json_response = connect_to_endpoint(url, headers)
     print(json.dumps(json_response, indent=4, sort_keys=True))
+    with open('twitter_data_json.txt', 'w') as outfile:
+        json.dump(json_response, outfile, indent=4, sort_keys=True)
 
 
 if __name__ == "__main__":
